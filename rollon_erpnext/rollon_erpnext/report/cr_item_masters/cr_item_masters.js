e7d6f96b9e58cef8907f7ec2c499c315b755d187
santoshbb <santosh.baburao@gmail.com>
2018-12-01 19:33:29 +0530

    CR Item Master edited

---
+++ rollon_erpnext/rollon_erpnext/report/cr_item_masters/cr_item_masters.js
@@ -0,0 +1,10 @@
+frappe.query_reports["CR Item Masters"] = {
+    "filters":[
+    {
+    "fieldname":"item_group",
+    "label":("Item Group"),
+    "fieldtype":"Link",
+    "options":"Item Group"
+    }
+]
+}
---
+++ rollon_erpnext/rollon_erpnext/report/cr_item_masters/cr_item_masters.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2013, Biradar Technologies and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import flt, getdate
+
+def execute(filters=None):
+	columns, data = [], []
+        columns=get_columns()
+        data=get_data(filters)
+#        conditions=get_conditions(filters)
+	return columns, data
+
+def get_columns():
+ return [
+  _("Process No") + ":Data:80",
+  _("Item Code") + ":Link/Item:140",
+  _("Item Name") + ":Data:270",
+  _("ROLLON Code") + ":Data:130",
+  _("Item Group") + ":Data:130",
+  _("description") + ":Data:250"
+ ]
+
+
+def get_data(filters):
+ if filters.get("item_group"):
+  item_group=filters.get("item_group")
+  return frappe.db.sql("""
+  SELECT
+  A.process_no,
+  A.name,
+  A.item_name,
+  A.rollon_code,
+  A.item_group,
+  A.description
+  
+  FROM
+  `tabItem` AS A
+  
+  WHERE
+  A.item_group=%(item_group)s  
+  ORDER BY A.rollon_code DESC, A.process_no DESC """ %(item_group), as_list=1)
+
+ else:
+  return frappe.db.sql("""
+  SELECT
+  A.process_no,
+  A.name,
+  A.item_name,
+  A.rollon_code,
+  A.item_group,
+  A.description
+  
+  FROM
+  `tabItem` AS A
+  
+  ORDER BY A.rollon_code DESC, A.process_no DESC """ ) 
+
