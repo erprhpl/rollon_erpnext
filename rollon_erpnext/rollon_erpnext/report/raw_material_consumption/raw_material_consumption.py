fa31dd7767fe6ee59a07fbeeacbfa10bbca37b42
santoshbb <santosh.baburao@gmail.com>
2019-01-03 16:54:34 +0530

    RM consumption report changed

--- rollon_erpnext/rollon_erpnext/report/raw_material_consumption/raw_material_consumption.py
+++ rollon_erpnext/rollon_erpnext/report/raw_material_consumption/raw_material_consumption.py
@@ -17,8 +17,10 @@ def get_columns(filters):
   if filters.get("details"):
     if filters.get("details")=="Less Details":
      return [
-     _("Raw Material Consumed") + ":Link/Item:230",
-     _("Qty Consumed") + ":Float:120"
+     _("Raw Material Consumed/Supplied") + ":Link/Item:230",
+     _("Qty Consumed/Supplied") + ":Float:180",
+     _("Item Manufactured/Received") + ":Link/Item:230",
+     _("Qty Manufactured/Received") + ":Float:180"
      ]
     else:
      return [
@@ -27,16 +29,14 @@ def get_columns(filters):
      _("Raw Material Consumed/ Supplied") + ":Data:220",
      _("Qty Supplied (Kg's)") + ":Float:120",
      _("Item Manufactured/Received") + ":Data:240",
-     _("Qty Received (No's)") + ":Float:130"
+     _("Qty Received (No's)") + ":Float:130",
+     _("==>") + ":Data:3",
+     _("Raw Material to be Consumed/Supplied") + ":Data:260",
+     _("BOM CF") + ":Float:60",
+     _("Qty to be Consumed/Supplied") +":Float:220",
+     _("Item Difference(0,1)") + ":Data:160",
+     _("Qty Difference(0,1)") + ":Data:120"
      ]
-
-#  elif (filters.get("supplier_balance")):
-#    return [
-#    _("Raw Material Supplied") + ":Data:150",
-#    _("Qty Supplied") + ":Float:100",
-#    _("Qty Consumed") + ":Float:150",
-#    _("Supplier Balance") + ":Float:150" 
-#    ]
 
 #------------------------------------------------------------------------------------------------------------------
 def get_data(filters):
@@ -56,27 +56,41 @@ def get_data(filters):
                    A.name,
                    case when C.item_group="Raw Material" then B.item_name end,
                    case when C.stock_uom="Kg" then B.qty end,
-                   if (C.item_group<>"Raw Material", B.item_name,B.item_name),
-                   B.qty
+                   "To Be Programmed",
+                   "B.qty",
+                   "=>",
+                   E.item_name,
+                   E.qty,
+                   B.qty*E.qty,
+                   if(B.item_name=E.item_name,0,1),
+                   if(B.qty=B.qty*E.qty,0,1)
+                                      
+
                    FROM
                    `tabStock Entry` AS A,
                    `tabStock Entry Detail` AS B,
-                   `tabItem` AS C
+                   `tabItem` AS C,
+                   `tabBOM` AS D,
+                   `tabBOM Item` AS E
 
                    WHERE
                    A.name=B.parent
                    && B.item_name=C.item_name
+                   && A.bom_no=D.name
+                   && D.name=E.parent
                    && A.posting_date >= '%s' && A.posting_date <= '%s' 
                    && C.item_group = '%s' 
                    && A.purpose = '%s'
-                   ORDER BY B.item_code ASC """  %(from_date,to_date,item_group,consumption_type), as_list=1)
+                   ORDER BY B.item_code ASC, A.name ASC """  %(from_date,to_date,item_group,consumption_type), as_list=1)
 #              1111111111111111111111111111111111111111111111111111111111111111111111111111111
                elif filters.get("details")=="Less Details":
 #              11111111111111111111111111111111111111111111111111111111111111111111111111111111
                    return frappe.db.sql("""
                    SELECT
                    B.item_name,
-                   sum(B.qty)
+                   sum(B.qty),
+                   "To Be Programed",
+                   "To be programed"
 
                    FROM                                         
                    `tabStock Entry` AS A,
@@ -97,7 +111,9 @@ def get_data(filters):
                    return frappe.db.sql("""
                    SELECT DISTINCT
                    D.item_name,
-                   sum(C.consumed_qty)
+                   sum(C.consumed_qty),
+                   B.item_name,
+                   sum(B.qty)
 
                    FROM
                    `tabPurchase Receipt` AS A,
@@ -111,7 +127,7 @@ def get_data(filters):
                    && D.name=C.rm_item_code
                    && A.posting_date >= '%s' && A.posting_date <= '%s'
                    && D.item_group='%s'
-                   GROUP BY D.item_name 
+                   GROUP BY B.item_name 
                    ORDER BY D.item_name ASC """ %(from_date,to_date,item_group), as_list=1)
 
                elif filters.get("details")=="More Details":
@@ -122,20 +138,30 @@ def get_data(filters):
                    D.item_name,
                    C.consumed_qty,
                    B.item_name,
-                   B.qty
+                   B.qty,
+                   "=>",
+                   F.item_name,
+                   F.qty,
+                   F.qty*B.qty,
+                   if(F.item_name=D.item_name,0,1),
+                   if(C.consumed_qty=(B.qty*F.qty),0,1)
 
                    FROM
                    `tabPurchase Receipt` AS A,
                    `tabPurchase Receipt Item` AS B,
                    `tabPurchase Receipt Item Supplied` AS C,
-                   `tabItem` AS D
+                   `tabItem` AS D,
+                   `tabBOM` AS E,
+                   `tabBOM Item` AS F
 
                    WHERE
                    A.name=B.parent
                    && A.name=C.parent
                    && D.name=C.rm_item_code
+                   && B.bom=E.name
+                   && E.name=F.parent
                    && A.posting_date >= '%s' && A.posting_date <= '%s'
                    && D.item_group='%s'
-                   ORDER BY D.item_name ASC """ %(from_date,to_date,item_group), as_list=1)
+                   ORDER BY D.item_name ASC,A.name ASC """ %(from_date,to_date,item_group), as_list=1)
 #       1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111 
 #-----------------------------------------------------------------------------------------------------------
