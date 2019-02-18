fe98c8d14d5ac6969eba5d5d4b4e6db754477603
Santosh Baburao <santosh.baburao@gmail.com>
2019-02-18 16:15:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in rollon_erpnext/__init__.py
+ # get version from __version__ variable in rollon_erpnext/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('rollon_erpnext/__init__.py', 'rb') as f:
+ with open('rollon_erpnext/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+ requirements = parse_requirements("requirements.txt", session="")
 
-setup(
+ setup(
 	name='rollon_erpnext',
 	version=version,
 	description='ERPNext customisation for Rollon Hydraulics',
